f4e5a034cdb4f7e292f41f6fb0faf60867eb5c31
facchiR <Roz@DESKTOP-GJS3F47.lan>
2017-02-15 16:25:32 +0100

    new colors for the page

--- public_html/js/site.js
+++ public_html/js/site.js
@@ -1,37 +1,37 @@
- $(document).ready(function(){
-    $("#flip1").click(function(){
-        $("#panelN1").slideToggle("slow");
+$(document).ready(function () {
+    $("#flip1").click(function () {
+        $("#panelN1").slideToggle("normal");
     });
-    $("#flip2").click(function(){
-        $("#panelN2").slideToggle("slow");
+    $("#flip2").click(function () {
+        $("#panelN2").slideToggle("normal");
     });
-    
+
     // Add smooth scrolling on all links inside the navbar
-  $("#myScrollspy a").on('click', function(event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+    $("#myScrollspy a").on('click', function (event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
 
-      // Store hash
-      var hash = this.hash;
+            // Store hash
+            var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    }  // End if
-  });
-    
-    
-    
-    
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        }  // End if
+    });
+
+
+
+
 });
-        
-        
+
+
     
