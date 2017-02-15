d0a0a7307556d8416edec4dc3b8b9a66127e1a75
facchiR <Roz@DESKTOP-GJS3F47.lan>
2017-02-15 11:36:26 +0100

    added animation on scrollspy added sliding effect in sidebar

--- public_html/js/site.js
+++ public_html/js/site.js
@@ -1,43 +1,37 @@
-  $(document).ready(function() {
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-                
-	/*Menu-toggle*/
-//    $("#menu-toggle").click(function(e) {
-//        e.preventDefault();
-//        $("#wrapper").toggleClass("active");
-//    });
+ $(document).ready(function(){
+    $("#flip1").click(function(){
+        $("#panelN1").slideToggle("slow");
+    });
+    $("#flip2").click(function(){
+        $("#panelN2").slideToggle("slow");
+    });
+    
+    // Add smooth scrolling on all links inside the navbar
+  $("#myScrollspy a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
-    /*Scroll Spy*/
-//    $('body').scrollspy({ target: '#spy', offset:80});
+      // Store hash
+      var hash = this.hash;
 
-    /*Smooth link animation*/
-//    $('a[href=#])').click(function() {
-//        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
-//
-//            var target = $(this.hash);
-//            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-//            if (target.length) {
-//                $('html,body').animate({
-//                    scrollTop: target.offset().top
-//                }, 1000);
-//                return false;
-//            }
-//        }
-//    });
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }  // End if
+  });
+    
+    
+    
+    
+});
         
-        });
+        
+    
