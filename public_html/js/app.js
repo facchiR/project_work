65e16d6d8f80e420c3f36d4bcb33346ffcb60483
Roz <Roz@DESKTOP-GJS3F47.lan>
2017-03-14 14:49:00 +0100

    index.php divided in several pages charged onload

---
+++ public_html/js/app.js
@@ -0,0 +1,48 @@
+var a = {};
+a.pfolder = 'pgs/';
+a.id = 0;
+a.msg = function(m){
+    alert(m);
+};
+a.load = function(p){
+    a.pg = p;
+    try{
+        $('.dyn').load(a.pfolder+p+'.html');
+    }catch(e){
+        console.log('Error:',e);
+        a.notFound();
+        return false;
+    }
+};
+a.notFound = function(){
+    $('.dyn').load(a.pfolder+'notfound.html');
+};
+
+a.startApp = function () {
+	//inibisco la cache sui dati che carico da remoto
+	$.ajaxSetup({cache:false})
+	
+	//rotte 
+	Path.map("#/home").to(function () {
+		a.load('home');
+	}) ;
+	Path.map("#/:pag(/:id)").to(function () {
+		//se nell'URL è stato specificato un id, lo memorizzo in una variabile figlia di a.p, che ha il nome della pagina corrente ed il suffisso _id (es: post_id)
+		a.id = this.params.id;
+		//permette alle pagine di caricare dati
+		a.notLoad=false;
+		//carico la pagina specificata nel parametro "pag"
+		a.load(this.params.pag);
+	});	
+	Path.root("#/home"); // rotta di default: se non viene specificata alcuna route nell'url carica la home
+	Path.rescue(a.notFound); // se la rotta specificata non è valida esegue la funzione a.notFound
+	Path.listen(); //ENTRA IN AZIONE!
+	$('#int1').load('int1.html');
+	$('#sec2').load('sec2.html');
+	$('#sec3').load('sec3.html');
+	$('#sec4').load('sec4.html');
+	$('#sec5').load('sec5.html');
+	};
+
+$(a.startApp);
+
